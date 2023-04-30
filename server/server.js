eaf4cb5c71bdf906dd2c5392debbcbf3bdf893aa
Miroslav Sekula <miroslavsekula@Miroslavs-MacBook-Air.local>
2023-04-30 22:04:45 +0100

    first commit

--- server/server.js
+++ server/server.js
@@ -44,4 +44,4 @@ app.post('/', async (req, res) => {
     }
 })
 
-app.listen(4000, () => console.log('Server is running on port http://localhost:4000'));
+app.listen(5000, () => console.log('Server is running on port http://localhost:5000'));
